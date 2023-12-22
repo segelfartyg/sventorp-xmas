1e8089980e0cd4b3bd7da9cb82eed28367674cb0
Samuel Swarén <samuel.swaren@sogeti.com>
2023-12-22 17:02:32 +0100

    Init: added core files

---
+++ xmas.js
@@ -0,0 +1,124 @@
+let firstDiceValue = 1
+let secondDiceValue = 1
+
+let firstDiceImageSrc = document.getElementById("firstDice");
+let secondDiceImageSrc = document.getElementById("secondDice");
+
+
+
+function throwTwo(){
+
+    var audio = new Audio('./sounds/diceroll.mp3');
+    audio.play();
+    scrambleDice();
+
+    let scrambleInterval = setInterval(() => {
+            scrambleDice();
+    }, 50);
+
+    setTimeout(() => {
+
+        launchOrdinaryGrafitti()
+        let firstDiceValue = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+        firstDiceImageSrc.src = `./1dice${firstDiceValue}.png`
+        
+       
+    
+        let secondDiceValue = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+        secondDiceImageSrc.src = `./1dice${secondDiceValue}.png`
+
+        clearInterval(scrambleInterval);
+    
+        if(firstDiceValue == 6 || secondDiceValue == 6){
+    
+            launchStarsGrafitti();
+            showMessage("Ta ett paketjävel!!!!! SNURRA HÖGER");
+        }
+        
+    }, 500);
+   
+
+}
+
+function scrambleDice(){
+        let firstDiceValue = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+        firstDiceImageSrc.src = `./1dice${firstDiceValue}.png`
+        
+        let secondDiceValue = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+        secondDiceImageSrc.src = `./1dice${secondDiceValue}.png`
+
+}
+
+function showMessage(message){
+    let overlay = document.getElementById("overlay");
+    overlay.style.display = "block";
+    setTimeout(() => {
+        overlay.style.display = "none";
+    }, 1000);
+    let popup = document.getElementById("popup");
+    popup.innerHTML = message;
+}
+
+
+// CONFETTI STUFF
+function launchOrdinaryGrafitti(){
+
+const end = Date.now() + 2 * 1000;
+const colors = ["#bb0000", "#ffffff"];
+
+(function frame() {
+  confetti({
+    particleCount: 2,
+    angle: 60,
+    spread: 55,
+    origin: { x: 0 },
+    colors: colors,
+  });
+
+  confetti({
+    particleCount: 2,
+    angle: 120,
+    spread: 55,
+    origin: { x: 1 },
+    colors: colors,
+  });
+
+  if (Date.now() < end) {
+    requestAnimationFrame(frame);
+  }
+})();
+
+}
+
+
+function launchStarsGrafitti(){
+    const defaults = {
+        spread: 360,
+        ticks: 50,
+        gravity: 0,
+        decay: 0.94,
+        startVelocity: 30,
+        shapes: ["star"],
+        colors: ["FFE400", "FFBD00", "E89400", "FFCA6C", "FDFFB8"],
+      };
+      
+      function shoot() {
+        confetti({
+          ...defaults,
+          particleCount: 40,
+          scalar: 1.2,
+          shapes: ["star"],
+        });
+      
+        confetti({
+          ...defaults,
+          particleCount: 10,
+          scalar: 0.75,
+          shapes: ["circle"],
+        });
+      }
+      
+      setTimeout(shoot, 0);
+      setTimeout(shoot, 100);
+      setTimeout(shoot, 200);
+}
